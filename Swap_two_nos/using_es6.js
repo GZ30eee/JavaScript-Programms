6f1ada232d3f4a53f90aba632ae61d6b98c974d0
Ghanshyamsinh Zala <130747789+GZ30eee@users.noreply.github.com>
2024-02-27 00:24:31 +0530

    JS Programs

---
+++ Swap_two_nos/using_es6.js
@@ -0,0 +1,11 @@
+//JavaScript program to swap two variables
+
+//take input from the users
+let a = prompt('Enter the first variable: ');
+let b = prompt('Enter the second variable: ');
+
+//using destructuring assignment
+[a, b] = [b, a];
+
+console.log(`The value of a after swapping: ${a}`);
+console.log(`The value of b after swapping: ${b}`);
